fc5ee262c0f0cd4956e97d249b86a012126ec5e0
Asuma <slavikden1616@gmail.com>
2023-02-05 00:13:18 +0200

    spring security

--- src/main/java/com/workshop/asuma/auth/AuthenticationController.java
+++ src/main/java/com/workshop/asuma/auth/AuthenticationController.java
@@ -2,20 +2,19 @@ package com.workshop.asuma.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
-
-
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
